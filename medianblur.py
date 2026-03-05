8070d74a6c3054f637c948e57c5dc4b87d11c6a5
ashwinii <ashwinipowar2111@gmail.com>
2026-03-05 16:55:34 +0530

    Add files via upload

---
+++ medianblur.py
@@ -0,0 +1,7 @@
+import cv2
+image=cv2.imread("camel.jpg")
+blurred=cv2.medianBlur(image,5)
+cv2.imshow("original image",image)
+cv2.imshow("blurred image",blurred)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
