9ba48ff428f8484ea69ea68fc6ca8255195acf58
ashwinii <ashwinipowar2111@gmail.com>
2026-03-06 22:47:42 +0530

    Add files via upload

---
+++ sharpenning.py
@@ -0,0 +1,18 @@
+import numpy as np
+import cv2
+
+image = cv2.imread("blur.png")
+
+sharpen_kernel = np.array([
+    [0,-1,0],
+    [-1,5,-1],
+    [0,-1,0]
+])
+
+sharpened = cv2.filter2D(image, -1, sharpen_kernel)
+
+cv2.imshow("original image", image)
+cv2.imshow("sharpened image", sharpened)
+
+cv2.waitKey(0)
+cv2.destroyAllWindows()
